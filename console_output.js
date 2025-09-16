0031669cf626861f4f8bce5cde0fbf389d3d0468
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-17 00:29:40 +0530

    Update console_output.js

--- console_output.js
+++ console_output.js
@@ -2,9 +2,9 @@ module.exports = {
 	r: console.log,
 	lang : {
 		en : {
-			start : '💽 Starting Whatsapp Bot...',
+			start : '💽 Starting pasiya-md Whatsapp Bot...',
 			version : '🌐 Firefox Whatsapp Web Version ',
-			started : '👨‍💻 Manoj Multi-Device Whatsapp Bot Is Activated!',
+			started : '👨‍💻 POWERED_BUY PASIYA-MD  PRIME Whatsapp Bot Is Activated!',
 			logout : 'Your Session Is Logout From Whatsapp.. Please Rescan Qr And Update The Session',
 			login_db : '🌏 Loging To DataBase...',
 			logged_db : '✅ Successfully Connected Your Database',
@@ -12,7 +12,7 @@ module.exports = {
 			updated_db : '✅ Database Updated Successfull',
 			ins_cmd : '🌐 Installing Commands...',
 			insd_cmd : '✅ Installation Successfull...',
-			session_error : 'Your Session Is Invalid... Please Rescan And Update The ENV ( https://ravindu01manoj.github.io/ravindu01manoj/qr )',
+			session_error : 'Your Session Is Invalid... Please Rescan And Update The ENV ( https:/qr )',
 			db_invalid : 'Your Database Url Is Invalid... Please Input Valid Mongodb Database Url As env [DBURI]'
 		}
 	}
