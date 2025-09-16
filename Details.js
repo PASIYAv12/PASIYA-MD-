a30a4528f906a7573025c324cf011fbd6f1f0457
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-17 00:31:43 +0530

    Update Details.js

--- Details.js
+++ Details.js
@@ -8,9 +8,9 @@ Youtube: https://youtube.com/c/TechToFuture
 Coded By Ravindu Manoj
 */
 module.export = {
-	name: 'Manoj Multi-device Whatsapp Bot',
+	name: 'pasiya-md Whatsapp Bot',
 	version: require('./pre-package.json').version,
 	branch: 'master',
 	author: require('./pre-package.json').author,
-	ravindumanoj_api_key : process.env.Ravindu_Manoj_Api || 'YourApiKey' // go to https://ravindumanoj.ml/api
+	ravindumanoj_api_key : process.env.pasidu_sampath_Api || 'YourApiKey' // go to https://ravindumanoj.ml/api
 }
