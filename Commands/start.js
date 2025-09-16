9d3b7b0b4447b6bca96fe0d1eb271278d0b7bf9d
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-17 02:21:15 +0530

    Update start.js

--- Commands/start.js
+++ Commands/start.js
@@ -1,22 +1,21 @@
 /*
-Manoj Md Whatsapp Bot
+pasiya-md  whats apps bot
 
-website: https://ravindumanoj.ml
-Telegram: https://t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
-Youtube: https://youtube.com/c/TechToFuture
+website: 
+Telegram: 
+Facebook: 
+Youtube: 
 
-Coded By Ravindu Manoj
+Coded by pasidu sampath
 */
+const { BootBot } = pasidu
 
-const { BootBot } = Ravindu
-
-Manoj.shutdown.start = async(core) => {
+sampath.shutdown.start = async(core) => {
 	BootBot('shutdown')
 	await core.send(string().start.shutdown.done)
 }
 
-Manoj.start.start = async(core) => {
+pasiya.start.start = async(core) => {
 	await core.send(string().start.start.boot)
 	BootBot('start')
 	await core.send(string().start.start.done)
