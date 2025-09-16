ad74f415d1b984a2b3849d12d3726ab0ebff31ce
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-17 01:58:19 +0530

    Update crypt.js

--- Commands/crypt.js
+++ Commands/crypt.js
@@ -1,12 +1,12 @@
 /*
-Manoj Md Whatsapp Bot
+pasiya-md  whats apps bot
 
-website: https://ravindumanoj.ml
-Telegram: https://t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
-Youtube: https://youtube.com/c/TechToFuture
+website: 
+Telegram: 
+Facebook: 
+Youtube: 
 
-Coded By Ravindu Manoj
+Coded by pasidu sampath
 */
 var { ravindumanoj_api_key } = require('../Details.js')
 var Api_url = 'https://api pasidu-sampath.ml/'
