399bd274393090e17018d9711a0bde36e5355984
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-17 02:25:39 +0530

    Update viewOnce.js

--- Commands/viewOnce.js
+++ Commands/viewOnce.js
@@ -1,16 +1,15 @@
 /*
-Manoj Md Whatsapp Bot
+pasiya-md  whats apps bot
 
-website: https://ravindumanoj.ml
-Telegram: https://t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
-Youtube: https://youtube.com/c/TechToFuture
+website: 
+Telegram: 
+Facebook: 
+Youtube: 
 
-Coded By Ravindu Manoj
+Coded by pasidu sampath
 */
 
-
-Manoj.antivo.start = async(core) => {
+pasiya.antivo.start = async(core) => {
 	var data = await core.download()
 	if(data.buffer && (data.type == 'image' || data.type == 'video') && core.moredata.isviweOnce) {
 		await core.reply(string().viweOnce.doing)
